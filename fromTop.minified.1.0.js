741ca13e08f080b02ea77c6980d3df5803af4983
Jacob Gray <jacob@debonairstudios.com>
2015-04-08 12:24:49 -0400

    Create fromTop.minified.1.0.js

---
+++ fromTop.minified.1.0.js
@@ -0,0 +1,2 @@
+//fromTop() Version 1.0 jQuery function. 
+$(document).ready(function(){$.fn.fromTop=function(b,c){var a=$(window).scrollTop(),a=$(this).offset().top-a;window.scrollActivated=!1;window.scrollActivated=a<=b?!0:!1;c();return this}})(jQuery);
