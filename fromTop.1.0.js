041da41c9ea73c789f38c4f455c058a5fcdf51b1
Jacob Gray <jacob@debonairstudios.com>
2015-04-11 13:02:11 -0400

    Version 1.1 update
    Added config.log for de-buging purposes. Prints element viewport status
    ("In viewport/Not in viewport")to the console

--- fromTop.1.0.js
+++ fromTop.1.0.js
@@ -1,13 +1,20 @@
 //fromTop() Version 1.1 jQuery function. 
 jQuery(function ($) {
-$.fn.fromTop = function(config){
-	var windowTop  = $(window).scrollTop(), 
-	targetTop = $(this).offset().top, 
-	distance = (targetTop - windowTop);
-	if(distance <= config.start){
-	config.success();
+	function log(data){
+		 console.log(data);
 	}
-	else config.fail();
-	return this;
+	$.fn.fromTop = function(config){
+		var windowTop  = $(window).scrollTop(), 
+		targetTop = $(this).offset().top, 
+		distance = (targetTop - windowTop);
+		if(distance <= config.start){
+			config.inside();
+			if(config.log==true) log("In Viewport");
+		}
+		else{
+			config.outside();
+			if(config.log==true) log("Not In Viewport");
+		}
+		return this;
 	}
 });
