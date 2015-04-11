2863731ff4a9fabc746a4f8fccec2f02d0b55d8c
Jacob Gray <jacob@debonairstudios.com>
2015-04-11 16:38:09 -0400

    Version 1.1 Update
    Updated functions, added enter and exit config

--- fromTop.1.0.js
+++ fromTop.1.0.js
@@ -7,13 +7,28 @@ jQuery(function ($) {
 		var windowTop  = $(window).scrollTop(), 
 		targetTop = $(this).offset().top, 
 		distance = (targetTop - windowTop);
+		window.enterViewpane;
+		if(!config.start){
+			console.warn("fromTop() WARNING: A height from top to fire function should be specified with the \"start\" configuration. Default has been set to 0");
+			config.start=0;
+		}
 		if(distance <= config.start){
-			config.inside();
-			if(config.log==true) log("In Viewport");
+			if(config.inside) config.inside();
+			if(config.enter && window.enterViewpane !==true){
+				window.enterViewpane=true;
+				config.enter();
+				if(config.log) if(config.log==true) log("fromTop() Enter: Element has entered the viewpane.");
+			}
+			if(config.log) if(config.log==true) log("fromTop() TRUE: Is within "+config.start+"px from top of window");
 		}
 		else{
-			config.outside();
-			if(config.log==true) log("Not In Viewport");
+			if(config.outside) config.outside();
+			if(config.exit && window.enterViewpane==true){
+				config.exit();
+				window.enterViewpane=false;
+				if(config.log) if(config.log==true) log("fromTop() Exit: Element has exited the viewpane.");
+			}
+			if(config.log) if(config.log==true) log("fromTop() FALSE: Is over "+config.start+"px from top of window");
 		}
 		return this;
 	}
