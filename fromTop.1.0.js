6ebbcc625f7a379d5ea44145b481da516dce5912
Jacob Gray <jacob@debonairstudios.com>
2015-04-11 00:05:04 -0400

    Version 1.1 update
    Added success and fail methods, as well as config.start instead of a
    number

--- fromTop.1.0.js
+++ fromTop.1.0.js
@@ -1,15 +1,13 @@
-//fromTop() Version 1.0 jQuery function. 
+//fromTop() Version 1.1 jQuery function. 
 jQuery(function ($) {
-$.fn.fromTop = function(fromTop, CF){
+$.fn.fromTop = function(config){
 	var windowTop  = $(window).scrollTop(), 
 	targetTop = $(this).offset().top, 
 	distance = (targetTop - windowTop);
-	window.scrollActivated = false;
-	if(distance <= fromTop){
-	window.scrollActivated=true;
+	if(distance <= config.start){
+	config.success();
 	}
-	else window.scrollActivated=false;
-	CF();
+	else config.fail();
 	return this;
 	}
 });
