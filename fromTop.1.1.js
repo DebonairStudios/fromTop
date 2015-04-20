6d0dc568b431e79bb9e0ab7f5434be9390a5a2c9
Jacob Gray <jacob@debonairstudios.com>
2015-04-20 07:43:38 -0400

    2nd part of last commit
    read the "Fixed quite a large bug :smile:" commit :stuck_out_tongue:

--- fromTop.1.1.js
+++ fromTop.1.1.js
@@ -10,6 +10,7 @@ jQuery(function ($) {
 		targetTop = $(this).offset().top, 
 		distance = (targetTop - windowTop),
 		end = config.end || -9*9*9*9*9*9,
+		currentwin=$(this).hasClass('fromTopinViewport');
 		if(!config.start){
 			console.warn("fromTop() WARNING: A height from top to fire function should be specified with the \"start\" configuration. Default has been set to 0");
 			config.start=0;
@@ -21,6 +22,7 @@ jQuery(function ($) {
 		if(distance <= config.start && distance >= end){
 			if(config.inside) config.inside(this);
 			if(config.enter && currentwin !==true){
+				$(this).addClass('fromTopinViewport');
 				config.enter(this);
 				if(config.log && config.log===true) log("fromTop() Enter: Element has entered the viewpane.");
 			}
@@ -30,6 +32,7 @@ jQuery(function ($) {
 			if(config.outside) config.outside(this);
 			if(config.exit && currentwin===true){
 				config.exit(this);
+				$(this).removeClass('fromTopinViewport');
 				if(config.log) if(config.log===true) log("fromTop() Exit: Element has exited the viewpane.");
 			}
 			if(config.log) if(config.log===true) log("fromTop() FALSE: Is "+distance+"px from top of window");
