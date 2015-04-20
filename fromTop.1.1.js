8161fe768dc65dabb20d6016359be3b25eecd5dc
Jacob Gray <jacob@debonairstudios.com>
2015-04-19 21:39:22 -0400

    Fixed a small bug
    You can now target the specific element that fromTop is applied to by
    putting a var in the function:` inside(element){
    $(element).addClass('Tada');}`

--- fromTop.1.1.js
+++ fromTop.1.1.js
@@ -20,18 +20,18 @@ jQuery(function ($) {
 			return false;
 		}
 		if(distance <= config.start && distance >= end){
-			if(config.inside) config.inside();
+			if(config.inside) config.inside(this);
 			if(config.enter && window.enterViewpane !==true){
 				window.enterViewpane=true;
-				config.enter();
+				config.enter(this);
 				if(config.log && config.log===true) log("fromTop() Enter: Element has entered the viewpane.");
 			}
 			if(config.log && config.log===true) log("fromTop() TRUE: Is "+distance+"px from top of window");
 		}
 		else{
-			if(config.outside) config.outside();
+			if(config.outside) config.outside(this);
 			if(config.exit && window.enterViewpane===true){
-				config.exit();
+				config.exit(this);
 				window.enterViewpane=false;
 				if(config.log) if(config.log===true) log("fromTop() Exit: Element has exited the viewpane.");
 			}
